5882327fccc8b1d75068af15e4a70cb6ccd552c3
Kaaminari <rraazzer@gmail.com>
2025-04-27 10:25:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,17 +17,3 @@ function enviarCodigoRecuperacao() {
         }
     });
 }
-async function autenticarGitHub() {
-  try {
-    const response = await fetch('http://localhost:5000/github-auth', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    const data = await response.json();
-    console.log('Autenticação:', data);
-  } catch (error) {
-    console.error('Erro:', error);
-  }
-}
